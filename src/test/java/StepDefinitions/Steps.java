2d21a08f889d34825f75f7c5680ba00eaf44e2e6
Daniel Maciuca <maciuca_d@yahoo.com>
2022-04-17 20:01:56 +0300

    Adding Steps

---
+++ src/test/java/StepDefinitions/Steps.java
@@ -0,0 +1,31 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+    @Given("he user is on login page")
+    public void he_user_is_on_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters the righ credentials")
+    public void the_user_enters_the_righ_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("hits submit button")
+    public void hits_submit_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user is logged in successfully")
+    public void the_user_is_logged_in_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
