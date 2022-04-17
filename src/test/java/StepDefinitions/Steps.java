e2735ba1c7516d77433073780c87fab5cec30a64
Daniel Maciuca <maciuca_d@yahoo.com>
2022-04-17 20:37:01 +0300

    Added code logic

--- src/test/java/StepDefinitions/Steps.java
+++ src/test/java/StepDefinitions/Steps.java
@@ -9,23 +9,22 @@ public class Steps {
 
     @Given("he user is on login page")
     public void he_user_is_on_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User is on login page");
     }
+
     @When("the user enters the right credentials")
     public void the_user_enters_the_right_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User enters right credentials");
     }
+
     @And("hits submit button")
     public void hits_submit_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User hits submit button");
     }
+
     @Then("the user is logged in successfully")
     public void the_user_is_logged_in_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User is logged in");
     }
 
 }
--- target/test-classes/firstTest.feature
+++ target/test-classes/firstTest.feature
@@ -1,6 +1,6 @@
 Feature: Web Login1
   Scenario: User should be able to login with valid credentials
     Given he user is on login page
-    When the user enters the righ credentials
+    When the user enters the right credentials
     And hits submit button
     Then the user is logged in successfully
