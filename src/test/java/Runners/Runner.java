4e186c13432c2d9ee8b17b5f07d12e5510bfb511
Daniel Maciuca <maciuca_d@yahoo.com>
2022-04-19 18:44:24 +0300

    All.

---
+++ src/test/java/Runners/Runner.java
@@ -0,0 +1,14 @@
+package Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/firstTest.feature",
+        glue = {"StepDefinitions"}
+
+)
+public class Runner {
+}
