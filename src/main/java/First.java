8f08ff3d377d935326857085c51c60a7c0a425b5
Daniel Maciuca <maciuca_d@yahoo.com>
2023-06-17 00:18:20 +0300

    Test Runner Class location added in pom xml

---
+++ src/main/java/First.java
@@ -0,0 +1,10 @@
+public class First {
+
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
