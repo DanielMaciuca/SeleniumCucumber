6fdf6153be1d1cfe862b6b3c14c15cf20a4ebb48
Daniel Maciuca <maciuca_d@yahoo.com>
2023-07-03 08:37:20 +0300

    Test Runner Class location added in pom xml

--- src/main/java/HasMap/HashMapExample.java
+++ src/main/java/HasMap/HashMapExample.java
@@ -1,4 +1,40 @@
 package HasMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        HashMap<String, Integer> happy = new HashMap<>();
+
+        happy.put("a", 10);
+        happy.put("b", 20);
+        happy.put("c", 30);
+        happy.put("d", 40);
+
+        System.out.println(happy.get("b"));
+
+        System.out.println(happy);
+
+        HashMap<String, String> fun = new HashMap<>();
+
+        fun.put("Boby99", "fluffy!");
+        fun.put("Gigi", "porto");
+		fun.put("Nicu", "mona100");
+		fun.put("Mihai", "mona100");
+        fun.remove("Gigi");
+
+        System.out.println(fun);
+
+        System.out.println(fun.replace("Mihai", "monaUpdated"));
+
+		System.out.println(fun);
+
+    }
 }
