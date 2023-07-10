d24af59666d7ddf3958c93ad343da116e753311f
Daniel Maciuca <maciuca_d@yahoo.com>
2023-07-10 22:31:55 +0300

    HasMap example

--- src/main/java/Parsing/ParsingExample.java
+++ src/main/java/Parsing/ParsingExample.java
@@ -1,6 +1,29 @@
 package Parsing;
 
-public class ParsingExample {
+import java.util.HashMap;
 
+public class ParsingExample {
+    public static void main(String[] args) {
+
+//Converting a String to an int
+        String s = "102";
+
+        System.out.println(s + 4);
+
+        int b =  Integer.parseInt(s);
+
+        System.out.println(b + 4);
+
+        System.out.println(Integer.parseInt(s) + 4);
+
+//Converting an int to a String
+
+        int a = 100;
+
+        System.out.println(a);
+
+        System.out.println(Integer.toString(a) + 100);
+
+    }
 
 }
