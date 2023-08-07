f79301ff8d08b5f2a2159b866166941ca84cb561
Daniel Maciuca <maciuca_d@yahoo.com>
2023-08-07 22:26:19 +0300

    HasMap example

---
+++ src/main/java/Variables/StaticExample.java
@@ -0,0 +1,23 @@
+package Variables;
+
+class StaticExample {
+    static int staticField;
+
+    public static void main(String[] args) {
+        StaticExample instance1 = new StaticExample();
+        StaticExample instance2 = new StaticExample();
+
+        // Assign different values to the static field
+        instance1.staticField = 10;
+        instance2.staticField = 20;
+
+        System.out.println(instance1.staticField);  // Output: 20
+        System.out.println(instance2.staticField);  // Output: 20
+        System.out.println(StaticExample.staticField);    // Output: 20
+    }
+}
+
+
+
+
+
---
+++ src/main/java/Variables/Variables.java
@@ -0,0 +1,24 @@
+package Variables;
+
+class Port{
+
+    String name = "Cici";
+
+
+
+}
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        Port myObject1 = new Port();
+        Port myObject2 = new Port();
+
+        System.out.println(myObject1.name);
+        System.out.println(myObject2.name);
+        System.out.println(myObject1.name);
+
+    }
+
+}
