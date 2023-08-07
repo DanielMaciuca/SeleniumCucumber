7d710e3133157117afbdb932529ab15c6c030380
Daniel Maciuca <maciuca_d@yahoo.com>
2023-08-07 22:59:40 +0300

    HasMap example

---
+++ src/main/java/HasMap/RealHashMapExample.java
@@ -0,0 +1,27 @@
+package HasMap;
+
+import java.util.HashMap;
+
+public class RealHashMapExample {
+
+    public static String STATUS;
+
+    public static void main(String[] args) {
+
+        myMethod("Connection1");
+
+    }
+
+    public static void myMethod(String connection) {
+
+        HashMap<String, String> happy = new HashMap<>();
+
+        happy.put("connection", connection);
+        happy.put(STATUS, "Enable Connection");
+
+        System.out.println(happy.get("connection"));
+        System.out.println(happy.get(STATUS));
+    }
+}
+
+
--- src/main/java/Variables/Variables.java
+++ src/main/java/Variables/Variables.java
@@ -4,7 +4,7 @@ class Port{
 
     String name = "Cici";
 
-
+    static int ceva;
 
 }
 
@@ -16,8 +16,19 @@ public class Variables {
         Port myObject2 = new Port();
 
         System.out.println(myObject1.name);
-        System.out.println(myObject2.name);
+
+        myObject1.ceva = 55;
+
+        System.out.println(myObject1.ceva);
+
+        myObject2.name = "Sarpe";
+
+        myObject2.ceva = 66;
+
         System.out.println(myObject1.name);
+        System.out.println(myObject2.ceva);
+        System.out.println(myObject2.name);
+        System.out.println(myObject1.ceva);
 
     }
 
