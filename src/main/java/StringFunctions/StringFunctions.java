cc7036f828c615276ffcf16fcd692cef241b0f58
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-08 23:44:23 +0200

    HasMap example

--- src/main/java/StringFunctions/StringFunctions.java
+++ src/main/java/StringFunctions/StringFunctions.java
@@ -5,13 +5,16 @@ public class StringFunctions {
 
     public static void main(String[] args) {
 
+        // String is a complex data type
+        // It is actually a class that can be instantiated
+
         //Substring function
 
         String[] date = {"17.01.1985-100", "21.02.1956-200"};
 
         System.out.println(date[0]);
 
-        String formattedDate = date[0].substring(0,2);
+        String formattedDate = date[0].substring(0, 2);
 
         System.out.println(formattedDate);
 
@@ -34,6 +37,17 @@ public class StringFunctions {
         System.out.println(space[1]);
 
         System.out.println(space[2]);
+
+        //IndexOf function
+
+        String index = "This is only a string";
+        System.out.println(index.indexOf("s"));
+
+        //Replace function
+
+        String myReplaceString = "Ala bala portocala";
+        String resultAfterReplace = myReplaceString.replace("bala", "portocala");
+        System.out.println(resultAfterReplace);
 
     }
 
