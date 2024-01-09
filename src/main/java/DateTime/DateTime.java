dce5e228b7581a37efe6feadf2154ada110b1c66
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-09 12:20:03 +0200

    HasMap example

---
+++ src/main/java/DateTime/DateTime.java
@@ -0,0 +1,15 @@
+package DateTime;
+
+import java.util.Date;
+
+public class DateTime {
+
+    public static void main(String[] args) {
+
+        Date now = new Date();
+
+        System.out.println(now);
+
+
+    }
+}
