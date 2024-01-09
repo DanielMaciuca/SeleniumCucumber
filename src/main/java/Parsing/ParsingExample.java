742cdc957964dabb7a822d7d6ef92d00834d853b
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-09 11:29:47 +0200

    HasMap example

--- src/main/java/Parsing/ParsingExample.java
+++ src/main/java/Parsing/ParsingExample.java
@@ -3,19 +3,19 @@ package Parsing;
 public class ParsingExample {
     public static void main(String[] args) {
 
-//Converting a String to an int
+        //Converting a String to an int
 
         String s = "102";
 
         System.out.println(s + 4);
 
-        int b =  Integer.parseInt(s);
+        int b = Integer.parseInt(s);
 
         System.out.println(b + 4);
 
         System.out.println(Integer.parseInt(s) + 4);
 
-//Converting an int to a String
+        //Converting an int to a String
 
         int a = 100;
 
