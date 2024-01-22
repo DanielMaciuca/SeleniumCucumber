63263e06beba5f18fddb1b4de3d1947aee16274b
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-22 17:36:56 +0200

    re1

--- src/main/java/ComparisonOperators/ComparisonOperators.java
+++ src/main/java/ComparisonOperators/ComparisonOperators.java
@@ -4,17 +4,23 @@ public class ComparisonOperators {
 
     public static void main(String[] args) {
 
-    // == (Equal to)
-    // != (Not equal to)
-    // > (Greater than)
-    // < (Less than)
-    // >= (Greater than or equal to)
-    // <= (Less than or equal to)
+        //These operators are used to compare values and return a boolean result: true or false
+
+        // == (Equal to)
+        // != (Not equal to)
+        // > (Greater than)
+        // < (Less than)
+        // >= (Greater than or equal to)
+        // <= (Less than or equal to)
 
         int x = 5;
         int y = 5;
 
-        System.out.println(x == y);
+       boolean result = x == y;
+
+        System.out.println(result);
+
+
 
     }
 }
--- src/main/java/LogicalOperators/LogicalOperators.java
+++ src/main/java/LogicalOperators/LogicalOperators.java
@@ -4,5 +4,13 @@ public class LogicalOperators {
 
     public static void main(String[] args) {
 
+//        Logical AND (&&): Returns true if both conditions are true.
+//        Logical OR (||): Returns true if at least one condition is true.
+//        Logical NOT (!): Returns the opposite boolean value. If the condition is true, ! operator makes it false; if it's false, ! operator makes it true
+
+        int x = 10;
+
+        boolean y = x > 10 && x < 10;
+
     }
 }
