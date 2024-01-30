d153fbd77514eb5a92031974c94f27344621fa8c
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-30 21:54:56 +0200

    re1

---
+++ src/test/java/Runner/justAClass.java
@@ -0,0 +1,23 @@
+package Runner;
+
+import org.junit.Test;
+
+public class justAClass {
+
+    //    WebDriver driver = new ChromeDriver();
+    @Test
+    public void myMethod() {
+
+        System.out.println("First message");
+
+//        driver.close();
+
+    }
+
+    public void mySecondMethod() {
+
+        System.out.println("Second message");
+
+    }
+
+}
