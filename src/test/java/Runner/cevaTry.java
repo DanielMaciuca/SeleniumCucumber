3a5a2cc79106e9f0f8638b42d0cf33c152471ba6
Daniel Maciuca <maciuca_d@yahoo.com>
2024-01-30 16:48:35 +0200

    re1

---
+++ src/test/java/Runner/cevaTry.java
@@ -0,0 +1,14 @@
+package Runner;
+
+import org.junit.Test;
+
+public class cevaTry {
+
+@Test
+    public void myMethod() {
+
+        System.out.println("This is just a simple message");
+
+    }
+
+}
--- src/test/resources/features/ceva.feature
+++ src/test/resources/features/ceva.feature
@@ -1,6 +1,5 @@
 
 Feature: ceva
 
-@Run
   Scenario: portocala
     Given check if <100> equal <100>
