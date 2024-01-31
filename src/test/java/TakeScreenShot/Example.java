3b28cb6d79494a3d1a1c59ca4f64879dcfad76d3
Daniel Maciuca <maciuca_d@yahoo.com>
2024-02-01 00:06:24 +0200

    Dependency update

---
+++ src/test/java/TakeScreenShot/Example.java
@@ -0,0 +1,47 @@
+package TakeScreenShot;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.amazon.com/");
+
+        TakesScreenshot screenshot = ((TakesScreenshot)driver);
+
+        File fileSource = screenshot.getScreenshotAs(OutputType.FILE);
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+
+        try {
+
+            FileUtils.copyFile(fileSource, new File("./screenshots/"+ timeStamp + ".png" ));
+        }
+
+        catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        System.out.println("Test was failed, screenshot make");
+
+    }
+}
---
+++ src/test/java/utils/BaseClass.java
@@ -0,0 +1,19 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseClass {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.amazon.com/");
+    }
+}
