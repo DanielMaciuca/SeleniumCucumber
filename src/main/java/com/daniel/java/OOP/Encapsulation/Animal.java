18e2f77cc22d43dd7995b040d61c7dfa2b5f338f
Daniel Maciuca <maciuca_d@yahoo.com>
2024-02-01 23:19:42 +0200

    Dependency update

---
+++ src/main/java/com/daniel/java/OOP/Encapsulation/Animal.java
@@ -0,0 +1,4 @@
+package com.daniel.java.OOP.Encapsulation;
+
+public class Animal {
+}
---
+++ src/main/java/com/daniel/java/OOP/Encapsulation/Main.java
@@ -0,0 +1,8 @@
+package com.daniel.java.OOP.Encapsulation;
+
+public class Main {
+
+    Animal animal = new Animal();
+
+//    animal.
+}
