a6afe97901130fc9aec828020bdfd5be74fda8dc
Daniel Maciuca <maciuca_d@yahoo.com>
2024-02-21 12:44:54 +0200

    Dependency update

--- src/main/java/com/daniel/java/Collections/ArrayList/ArrayListExample1.java
+++ src/main/java/com/daniel/java/Collections/ArrayList/ArrayListExample1.java
@@ -1,39 +1,33 @@
 package com.daniel.java.Collections.ArrayList;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListExample1 {
 
     public static void main(String[] args) {
 
-        //The main difference between an Array and an ArrayList is that ArrayList is flexible, you can remove/add elements, in Arrays this is not possible, you have a fix number of elements
-        //
+        // The main difference between an Array and an ArrayList is that ArrayList is flexible, you can remove/add elements how many elements you want, in Arrays this is not possible, you have a fix number of elements
+        // ArrayList only work with objects: String, Integer, etc.
+        // You can use ArrayList to print all the elements with a specific tag from the page
+        // List<String> fruitList vs ArrayList<String> fruitList -> the first is an interface and it is recommended to use, because it has a higher level of abstraction
 
-        ArrayList<String> fruitList = new ArrayList<>();
+        List<String> fruitList = new ArrayList<>();
 
         fruitList.add("Apple");
         fruitList.add("Orange");
         fruitList.add("Banana");
         fruitList.add("Mango");
 
+        System.out.println(fruitList);
 
-        //fruitList.remove("Banana");
-
-        //fruitList.clear();
+        // fruitList.remove("Banana");
+        // fruitList.clear();
 
         System.out.println(fruitList.contains("Apple123"));
 
         //get the value by index
         System.out.println(fruitList.get(2));
-
-        //Different from simple arrays
-        /*1. You can print all the values
-         * 2. You can add/remove how many items you want,
-         * the length is not fix.
-         */
-
-        System.out.println(fruitList);
-
 
     }
 }
--- src/main/java/com/daniel/java/Collections/ArrayList/ArrayListExamplePrintAllLinks.java
+++ src/main/java/com/daniel/java/Collections/ArrayList/ArrayListExamplePrintAllLinks.java
@@ -1,9 +1,13 @@
 package com.daniel.java.Collections.ArrayList;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class ArrayListExamplePrintAllLinks {
 
@@ -17,10 +21,13 @@ public class ArrayListExamplePrintAllLinks {
         driver.get("https://www.emag.ro/");
         driver.manage().window().maximize();
 
-
-
-
-
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        int i = links.size();
+        System.out.println(i);
+        for (int j = 0; j < i; j++) { //Printing the links
+            System.out.println(links.get(j).getText());
+        }
+        driver.close();
 
     }
 }
